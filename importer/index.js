d2f10cd926c40a31f194836592140e610e995d78
Kenneth Soh <kennethsohyq@gmail.com>
2021-01-03 16:30:36 +0800

    Update script to delete before reinserting data (#23)
    Took 5 minutes

--- importer/index.js
+++ importer/index.js
@@ -1,22 +1,55 @@
 const firestore = require('firestore-export-import');
 const serviceAccount = require('./serviceAccountKey.json');
+const admin = require('firebase-admin');
 const fs = require('fs');
 const path = require('path');
 
 firestore.initializeApp(serviceAccount);
+const firestoreAdmin = admin.firestore();
 
-let files = fs.readdirSync('./import');
+async function deleteCollection(db, collectionPath, batchSize) {
+    const query = db.collection(collectionPath).orderBy('__name__').limit(batchSize);
+    return new Promise((resolve, reject) => {deleteQueryBatch(db, query, resolve).catch(reject);});
+}
 
-const EXTENSION = ".json";
-let jsonFiles = files.filter((file) => {return path.extname(file).toLowerCase() === EXTENSION});
+async function deleteQueryBatch(db, query, resolve) {
+    const snapshot = await query.get();
+    if (snapshot.size === 0) { resolve(); return; } // When there are no documents left, we are done
 
-console.log(">>> Updating data...");
-jsonFiles.forEach(async (file) => {
-    let fn = file.replace(".json", "");
-    console.log(fn);
+    // Delete documents in a batch
+    const batch = db.batch();
+    snapshot.docs.forEach((doc) => {batch.delete(doc.ref);});
+    await batch.commit();
 
-    console.log(`>>> Restoring ${fn}...`);
-    await firestore.restore(`import/${file}`);
-    console.log(`>>> ${fn} restore completed!`);
-})
-console.log(">>> Data Update Complete!");
+    // Recurse on the next process tick, to avoid exploding the stack.
+    process.nextTick(() => {deleteQueryBatch(db, query, resolve);});
+}
+
+async function deleteCollections(fireStoreAdm) {
+    console.log(">>> Deleting Existing public weapon, characters and materials data");
+    await deleteCollection(fireStoreAdm, 'weapons', 50);
+    await deleteCollection(fireStoreAdm, 'characters', 50);
+    await deleteCollection(fireStoreAdm, 'materials', 50);
+    console.log(">>> Existing public data deleted successfully!")
+}
+
+async function restoreData() {
+    let files = fs.readdirSync('./import');
+
+    const EXTENSION = ".json";
+    let jsonFiles = files.filter((file) => {return path.extname(file).toLowerCase() === EXTENSION});
+
+    console.log(">>> Updating data...");
+    for (const file of jsonFiles) {
+        let fn = file.replace(".json", "");
+        console.log(fn);
+
+        console.log(`>>> Restoring ${fn}...`);
+        await firestore.restore(`import/${file}`);
+        console.log(`>>> ${fn} restore completed!`);
+    }
+    console.log(">>> Data Update Complete!");
+}
+
+console.log(">>> Starting Script");
+deleteCollections(firestoreAdmin).then(() => {restoreData().then(() => {console.log(">>> Script Complete");});});
