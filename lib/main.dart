606691696010f8656706f2dcb5554e65f5177f91
Kenneth Soh <kennethsohyq@gmail.com>
2021-01-06 18:24:25 +0800

    Character Info Page (#26)
    This is a basic information page for each character
    
    Took 32 minutes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:gi_weekly_material_tracker/widgets//bottomnav.dart';
 import 'package:gi_weekly_material_tracker/placeholder.dart';
+import 'package:gi_weekly_material_tracker/widgets/characters.dart';
 import 'package:gi_weekly_material_tracker/widgets/login.dart';
 import 'package:gi_weekly_material_tracker/widgets/materials.dart';
 
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
                   title: 'GI Materials Tracker',
                 )),
         GetPage(name: '/materials', page: () => MaterialInfoPage()),
-        GetPage(name: '/characters', page: () => PlaceholderPage()),
+        GetPage(name: '/characters', page: () => CharacterInfoPage()),
         GetPage(name: '/weapons', page: () => PlaceholderPage()),
       ],
     );
