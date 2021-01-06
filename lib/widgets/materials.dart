750f049c297ded0d2dc5807e14f7dcd4c3f94467
Kenneth Soh <kennethsohyq@gmail.com>
2021-01-06 17:28:14 +0800

    Implemented a basic information page for materials (#25)
    Took 1 hour 3 minutes

--- lib/widgets/materials.dart
+++ lib/widgets/materials.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:gi_weekly_material_tracker/models/grid.dart';
 import 'package:gi_weekly_material_tracker/placeholder.dart';
@@ -31,7 +32,8 @@ class _MaterialListGridState extends State<MaterialListGrid> {
             crossAxisCount: 3,
             children: snapshot.data.docs.map((document) {
               return GestureDetector(
-                onTap: () => Get.toNamed('/materials', arguments: [document.id, document.data()]),
+                onTap: () => Get.toNamed('/materials',
+                    arguments: [document.id, document.data()]),
                 child: GridData.getGridData(document),
               );
             }).toList(),
@@ -46,14 +48,16 @@ class MaterialInfoPage extends StatefulWidget {
 }
 
 class _MaterialInfoPageState extends State<MaterialInfoPage> {
-
   Map<String, dynamic> _infoData;
   String _infoId;
+
+  Color _rarityColor;
 
   @override
   void initState() {
     _infoData = Get.arguments[1];
     _infoId = Get.arguments[0];
+    _rarityColor = GridData.getRarityColor(_infoData['rarity']);
   }
 
   @override
@@ -61,7 +65,83 @@ class _MaterialInfoPageState extends State<MaterialInfoPage> {
     // TODO: implement build
     print(_infoId);
     //throw UnimplementedError();
-    return PlaceholderPage();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_infoData['name']),
+        backgroundColor: _rarityColor,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: _rarityColor,
+        onPressed: () => PlaceholderUtil.showUnimplementedSnackbar(context),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                GridData.getImageAssetFromFirebase(_infoData['image'],
+                    height: 64),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _infoData['type'],
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    RatingBar.builder(
+                      ignoreGestures: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      initialRating:
+                      double.tryParse(_infoData['rarity'].toString()),
+                      itemBuilder: (context, _) =>
+                          Icon(Icons.star, color: Colors.amber),
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Icon(Icons.location_pin),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: Text(_infoData['obtained']),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Icon(Icons.format_list_bulleted),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: Text(_infoData['description']),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
