867f51bd60882cde38ce408e9690e1e41cb5d8dc
Kenneth Soh <kennethsohyq@gmail.com>
2021-01-28 23:27:41 +0800

    Added ability for global tracking list to update bulk (#80)
    This is done by long clicking
    
    Took 11 minutes

--- lib/widgets/globaltracking.dart
+++ lib/widgets/globaltracking.dart
@@ -387,106 +387,297 @@ class _GlobalMaterialPageState extends State<GlobalMaterialPage> {
 
               return Container(
                 child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 64,
-                          width: 64,
-                          child: Stack(
-                            children: [
-                              GridData.getImageAssetFromFirebase(imageRef,
-                                  height: 48),
-                              Align(
-                                alignment: FractionalOffset.bottomLeft,
-                                child: Text(GridData.getRomanNumberArray(
-                                        extraAscensionRef - 1)
-                                    .toString()),
-                              ),
-                              Align(
-                                alignment: FractionalOffset.bottomRight,
-                                child: typeWidget,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width - 200,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                name,
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Spacer(),
-                        Column(
-                          children: [
-                            Text(
-                              "${_data["current"]}/${_data["max"]}",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: GridData.getCountColorBW(
-                                      _data["current"], _data["max"])),
-                            ),
-                            Row(
+                  child: InkWell(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 64,
+                            width: 64,
+                            child: Stack(
                               children: [
-                                ButtonTheme(
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 4.0, horizontal: 8.0),
-                                  //adds padding inside the button
-                                  materialTapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                  //limits the touch area to the button area
-                                  minWidth: 0,
-                                  //wraps child's width
-                                  height: 0,
-                                  //wraps child's height
-                                  child: FlatButton(
-                                    onPressed: () =>
-                                        TrackingData.decrementCount(key,
-                                            _data["type"], _data["current"]),
-                                    child: Icon(Icons.remove),
-                                  ),
+                                GridData.getImageAssetFromFirebase(imageRef,
+                                    height: 48),
+                                Align(
+                                  alignment: FractionalOffset.bottomLeft,
+                                  child: Text(GridData.getRomanNumberArray(
+                                          extraAscensionRef - 1)
+                                      .toString()),
                                 ),
-                                ButtonTheme(
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 4.0, horizontal: 8.0),
-                                  //adds padding inside the button
-                                  materialTapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                  //limits the touch area to the button area
-                                  minWidth: 0,
-                                  //wraps child's width
-                                  height: 0,
-                                  //wraps child's height
-                                  child: FlatButton(
-                                    onPressed: () =>
-                                        TrackingData.incrementCount(
-                                            key,
-                                            _data["type"],
-                                            _data["current"],
-                                            _data["max"]),
-                                    child: Icon(Icons.add),
-                                  ),
+                                Align(
+                                  alignment: FractionalOffset.bottomRight,
+                                  child: typeWidget,
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                      ],
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width - 200,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  name,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Column(
+                            children: [
+                              Text(
+                                "${_data["current"]}/${_data["max"]}",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: GridData.getCountColorBW(
+                                        _data["current"], _data["max"])),
+                              ),
+                              Row(
+                                children: [
+                                  ButtonTheme(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 4.0, horizontal: 8.0),
+                                    //adds padding inside the button
+                                    materialTapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                    //limits the touch area to the button area
+                                    minWidth: 0,
+                                    //wraps child's width
+                                    height: 0,
+                                    //wraps child's height
+                                    child: FlatButton(
+                                      onPressed: () =>
+                                          TrackingData.decrementCount(key,
+                                              _data["type"], _data["current"]),
+                                      child: Icon(Icons.remove),
+                                    ),
+                                  ),
+                                  ButtonTheme(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 4.0, horizontal: 8.0),
+                                    //adds padding inside the button
+                                    materialTapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                    //limits the touch area to the button area
+                                    minWidth: 0,
+                                    //wraps child's width
+                                    height: 0,
+                                    //wraps child's height
+                                    child: FlatButton(
+                                      onPressed: () =>
+                                          TrackingData.incrementCount(
+                                              key,
+                                              _data["type"],
+                                              _data["current"],
+                                              _data["max"]),
+                                      child: Icon(Icons.add),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
+                    onLongPress: () => _itemClickedAction(_data, key, {
+                      "img": imageRef,
+                      "asc": extraAscensionRef,
+                      "type": extraTypeRef
+                    }),
                   ),
                 ),
               );
             },
           );
         });
+  }
+
+  void _itemClickedAction(
+      Map<String, dynamic> data, String docId, Map<String, dynamic> extraData) {
+    print(docId);
+    String type = data["addedBy"];
+    String key =
+        (data["addedBy"] == "material") ? data["name"] : data["addData"];
+    _cntKey = docId;
+    switch (type) {
+      case "material":
+        _displayDialogMat("/materials", key, data);
+        break;
+      case "weapon":
+        _displayDialogNonMat("/weapons", key, data, extraData);
+        break;
+      case "character":
+        _displayDialogNonMat("/characters", key, data, extraData);
+        break;
+      default:
+        Util.showSnackbarQuick(
+            context, "Unsupported Action. Contact Developer");
+        break;
+    }
+  }
+
+  String _cntCurrent = "", _cntTotal = "", _cntKey = "", _cntType = "";
+  TextEditingController _textCurrentController = TextEditingController();
+  TextEditingController _textTotalController = TextEditingController();
+
+  void _displayDialogMat(
+      String navigateTo, String key, Map<String, dynamic> data) {
+    _cntCurrent = data["current"].toString();
+    _cntTotal = data["max"].toString();
+    _textCurrentController.text = _cntCurrent;
+    _textTotalController.text = _cntTotal;
+    showDialog(
+        context: context,
+        builder: (context) {
+          _cntType = data["type"];
+          return AlertDialog(
+            title: Text("Update tracked amount for ${_material["name"]}"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GridData.getImageAssetFromFirebase(_material["image"],
+                      height: 48),
+                  TextField(
+                    onChanged: (newValue) {
+                      _cntCurrent = newValue;
+                    },
+                    controller: _textCurrentController,
+                    decoration: InputDecoration(labelText: "Tracked"),
+                    keyboardType: TextInputType.number,
+                  ),
+                  TextField(
+                    onChanged: (newValue) {
+                      _cntTotal = newValue;
+                    },
+                    controller: _textTotalController,
+                    decoration: InputDecoration(labelText: "Max"),
+                    keyboardType: TextInputType.number,
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                child: Text('Info'),
+                onPressed: () {
+                  Get.back();
+                  Get.toNamed(navigateTo, arguments: [key]);
+                },
+              ),
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () => Get.back(),
+              ),
+              TextButton(
+                child: Text('Update'),
+                onPressed: _updateRecord,
+              ),
+            ],
+          );
+        });
+  }
+
+  void _displayDialogNonMat(String navigateTo, String key,
+      Map<String, dynamic> data, Map<String, dynamic> extraData) {
+    _cntCurrent = data["current"].toString();
+    _cntTotal = data["max"].toString();
+    _textCurrentController.text = _cntCurrent;
+    showDialog(
+        context: context,
+        builder: (context) {
+          _cntType = data["type"];
+          return AlertDialog(
+            title: Text("Update tracked amount for ${_material["name"]}"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GridData.getImageAssetFromFirebase(_material["image"],
+                          height: 48),
+                      _getSupportingWidget(extraData["img"], extraData["asc"],
+                          extraData["type"]),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Text("Max: $_cntTotal"),
+                  ),
+                  TextField(
+                    onChanged: (newValue) {
+                      _cntCurrent = newValue;
+                    },
+                    controller: _textCurrentController,
+                    decoration: InputDecoration(labelText: "Tracked"),
+                    keyboardType: TextInputType.number,
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                child: Text('Info'),
+                onPressed: () {
+                  Get.back();
+                  Get.toNamed(navigateTo, arguments: [key]);
+                },
+              ),
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () => Get.back(),
+              ),
+              TextButton(
+                child: Text('Update'),
+                onPressed: _updateRecord,
+              ),
+            ],
+          );
+        });
+  }
+
+  void _updateRecord() {
+    print("$_cntKey | $_cntType | $_cntCurrent | $_cntTotal");
+    TrackingData.setCount(_cntKey, _cntType, int.tryParse(_cntCurrent) ?? 0,
+        int.tryParse(_cntTotal) ?? 0);
+    Get.back();
+  }
+
+  Widget _getSupportingWidget(String image, int ascension, String type) {
+    if (image == null) return Container();
+    Widget typeWidget = SizedBox.shrink();
+    if (type != null)
+      typeWidget = Image.asset(
+        GridData.getElementImageRef(type),
+        height: 20,
+        width: 20,
+      );
+
+    return Container(
+      height: 48,
+      width: 48,
+      child: Stack(
+        children: [
+          GridData.getImageAssetFromFirebase(image, height: 32),
+          Align(
+            alignment: FractionalOffset.bottomLeft,
+            child: Text(
+              GridData.getRomanNumberArray(ascension - 1).toString(),
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.end,
+            ),
+          ),
+          Align(
+            alignment: FractionalOffset.bottomRight,
+            child: typeWidget,
+          ),
+        ],
+      ),
+    );
   }
 }
