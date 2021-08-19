6117dc3d6f68c25c913637a6300b0570b89f2669
Kenneth Soh <kennethsohyq@gmail.com>
2021-08-19 15:53:23 +0000

    First attempt at exposing Firebase Hosting (#168)

---
+++ util/hosting-branch.js
@@ -0,0 +1,58 @@
+// the .mjs file extention is only there so we can use nicer imports. The code is still normal JavaScript
+
+const {exec} = require('child_process');
+const {promisify} = require('util');
+const {writeFile} = require('fs/promises');
+
+// attempt to find an existing deploy for the given branch
+const getDeployUrl = async (branch) => {
+  const {stdout} = await promisify(exec)(`firebase hosting:channel:list --json`);
+  const {result} = JSON.parse(stdout);
+
+  return result.channels.find(deploy => deploy.name.split('/').pop() === branch)?.url;
+};
+
+// create a new hosting preview channel without deploying and return its' url
+const generateBranchDeployUrl = async (branch) => {
+  const {stdout} = await promisify(exec)(`firebase hosting:channel:create ${branch} --json`);
+  const {result} = JSON.parse(stdout);
+
+  return result.url;
+};
+
+// set the preview url in the CI env so future stages can use it
+const setEnv = async (url) => {
+  const envs = {
+    // this is the name of the variable that we'll be able to use through the rest of the CI process
+    // you can add more variables here if you need them
+    MY_DEPLOYED_URL: url,
+  };
+
+  const stringified = Object.entries(envs)
+    .reduce((prev, [key, value]) => `${prev}${key}=${value}\n`, '');
+
+  await writeFile('build.env', stringified);
+};
+
+// ensure that there is a preview channel ready for our deploy, and put its' url in the env
+const prepare = async (branch) => {
+  if (branch === process.env.CI_DEFAULT_BRANCH) {
+      branch = 'staging';
+  }
+  let url = await getDeployUrl(branch);
+
+  if (!url) {
+    url = await generateBranchDeployUrl(branch);
+  }
+
+  await setEnv(url);
+
+  console.log(`preparation complete: ${url}`);
+};
+
+prepare(process.argv[2] || process.env.CI_COMMIT_REF_SLUG)
+  .catch(e => {
+    console.error(e);
+
+    process.exit(1);
+  });
