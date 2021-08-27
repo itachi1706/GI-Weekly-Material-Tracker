a9de08edc6c0c656ca2a4c7954d68ff7caa09407
Kenneth Soh <kennethsohyq@gmail.com>
2021-08-27 14:46:21 +0000

    Added ability to manually add URL codes (#162)

--- util/addcode.js
+++ util/addcode.js
@@ -15,15 +15,31 @@ admin.initializeApp({
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 const db = admin.database();
 
-let asiacode = prompt("Enter Asia Code: ");
-let eucode = prompt("Enter EU Code (Leave empty if same as Asia): ");
-let nacode = prompt("Enter NA Code (Leave empty if same as Asia): ");
+let isCode = null;
+while (isCode == null) {
+    let isCodeStr = prompt("Is it a Promo Code or a URL? (code/url): ");
+    if (isCodeStr.toLowerCase() === 'code') isCode = true;
+    else if (isCodeStr.toLowerCase() === 'url') isCode = false;
+}
 
-if (!eucode) eucode = asiacode;
-if (!nacode) nacode = asiacode;
+let asiacode, eucode, nacode, codeurl, urlexpiry;
+let insertTime = new Date().getTime();
+
+if (isCode) {
+    asiacode = prompt("Enter Asia Code: ");
+    eucode = prompt("Enter EU Code (Leave empty if same as Asia): ");
+    nacode = prompt("Enter NA Code (Leave empty if same as Asia): ");
+
+    if (!eucode) eucode = asiacode;
+    if (!nacode) nacode = asiacode;
+} else {
+    codeurl = prompt("Enter Promo Code URL: ");
+    urlexpiry = prompt("Enter URL Expiry if known: ");
+    if (!urlexpiry) urlexpiry = 'Unknown';
+}
 
 let expiry = null;
-while (!expiry) {
+while (expiry == null) {
     let expireStr = prompt("Has it expired (y/n)?: ");
     if (expireStr.toLowerCase() === 'n') expiry = false;
     else if (expireStr.toLowerCase() === 'y') expiry = true;
@@ -44,17 +60,27 @@ let notifyStr = prompt("Do we notify users? (y/[n]): ");
 if (notifyStr.toLowerCase() === 'y') notify = true;
 
 // Add to DB
-let code = { dateString: dateStr, reward: description, expired: expiry, eu: eucode, na: nacode, asia: asiacode, date: date, notify: notify};
+let code, key;
+if (isCode) {
+    console.log('Crafting Code Data');
+    code = { dateString: dateStr, reward: description, expired: expiry, eu: eucode, na: nacode, asia: asiacode, date: date, notify: notify, type: 'code'};
+    key = code.asia;
+} else {
+    console.log('Craft URL Data');
+    code = { dateString: dateStr, reward: description, expired: expiry, url: codeurl, expiry: urlexpiry, date: date, notify: notify, type: 'url'};
+    key = `url_${insertTime}`;
+}
 
-console.log('Adding the following data to DB');
+
+console.log(`Adding the following data to DB with key ${key}`);
 console.log(code);
 
-async function addCode(code) {
-    await db.ref(`codes/${code.asia}`).update({...code, source: 'manual'}, (error) => {
+async function addCode(code, key) {
+    await db.ref(`codes/${key}`).update({...code, source: 'manual'}, (error) => {
         if (error) {
-            console.log(`ERROR: Fail to update code on DB (Key: ${code.asia}) [${error}]`);
+            console.log(`ERROR: Fail to update code on DB (Key: ${key}) [${error}]`);
         } else {
-            console.log(`Code key ${code.asia} added to DB`);
+            console.log(`Code key ${key} added to DB`);
         }
     });
 }
@@ -64,9 +90,10 @@ async function notifyUser(code) {
     // TODO: Notify users
 }
 
-addCode(code).then(async () => {
+addCode(code, key).then(async () => {
     if (code.notify) await notifyUser(code);
     console.log("Shutting down");
+    process.exit();
 })
 
 
