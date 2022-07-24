b2999da6185d78b5c33d1a29ea36d77919f477a0
Kenneth Soh <kenneth@itachi1706.com>
2022-07-24 17:11:52 +0000

    Update 2 new Github Actions (#379)
    Added ability to generate the Flutter Android APKs and Web App and upload them as artifacts

--- ciscripts/get_version.sh
+++ ciscripts/get_version.sh
@@ -3,4 +3,4 @@ echo "Updating versioning number for flutter app"
 APP_VERSION=$(cat ./VERSION)
 sed -i "s|version: 1.0.0+1|version: $APP_VERSION+$APP_BUILD_VER.b$CI_PIPELINE_IID|g" pubspec.yaml
 builddate="$(date +"%A, %d %B %Y %T %Z")"
-echo "Version $APP_VERSION ($APP_BUILD_VER.b$GIT_COMMIT_COUNT) Commit SHA #$CI_COMMIT_SHORT_SHA $APP_TYPE Application build on $builddate on branch $CI_COMMIT_REF_NAME with Pipleline $CI_PIPELINE_IID"
+echo "Version $APP_VERSION ($APP_BUILD_VER.b$GIT_COMMIT_COUNT) Commit SHA #$CI_COMMIT_SHORT_SHA $APP_TYPE Application build on $builddate on branch $CI_COMMIT_REF_NAME with Pipeline $CI_PIPELINE_IID"
