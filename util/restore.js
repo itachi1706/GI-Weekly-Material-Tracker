df17ca90a1dfffc53655d5be4c0cb0f3de00247a
Kenneth Soh <kenneth@itachi1706.com>
2022-07-30 05:22:20 +0000

    Reduce duplicate LOC

--- util/restore.js
+++ util/restore.js
@@ -3,27 +3,10 @@ const serviceAccount = require('./serviceAccountKey.json');
 const {getFirestore} = require('firebase-admin/firestore');
 const fs = require('fs');
 const path = require('path');
+const {deleteCollection} = require('./firebaseutil');
 
 initializeFirebaseApp(serviceAccount);
 const firestoreAdmin = getFirestore();
-
-async function deleteCollection(db, collectionPath, batchSize) {
-    const query = db.collection(collectionPath).orderBy('__name__').limit(batchSize);
-    return new Promise((resolve, reject) => {deleteQueryBatch(db, query, resolve).catch(reject);});
-}
-
-async function deleteQueryBatch(db, query, resolve) {
-    const snapshot = await query.get();
-    if (snapshot.size === 0) { resolve(); return; } // When there are no documents left, we are done
-
-    // Delete documents in a batch
-    const batch = db.batch();
-    snapshot.docs.forEach((doc) => {batch.delete(doc.ref);});
-    await batch.commit();
-
-    // Recurse on the next process tick, to avoid exploding the stack.
-    process.nextTick(() => {deleteQueryBatch(db, query, resolve);});
-}
 
 async function deleteCollections(fireStoreAdm) {
     console.log(">>> Deleting Existing public weapon, characters and materials data");
