84ebd54f8ca0aa4c968f7d767e7ca9c499022d91
Kenneth Soh <kenneth@itachi1706.com>
2024-05-21 02:26:31 +0800

    fix: Implemented fix for tab alignment on web

--- lib/helpers/grid.dart
+++ lib/helpers/grid.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_cached_image/firebase_cached_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gi_weekly_material_tracker/models/characterdata.dart';
@@ -649,5 +650,9 @@ class GridUtils {
     textData = textData.replaceAll('AoE §pPyro DMG§r', '§pAoE Pyro DMG§r');
 
     return textData;
+  }
+
+  static TabAlignment getTabAlignment() {
+    return kIsWeb ? TabAlignment.center : TabAlignment.start;
   }
 }
