f25c72ddfa290755107a2a2694cec0c301220eec
Kenneth Soh <kenneth@itachi1706.com>
2024-05-20 23:21:54 +0800

    feat: Upgraded to Flutter 3.19
    Refactored the Android gradle files as per migration guide (https://docs.flutter.dev/release/breaking-changes/flutter-gradle-plugin-apply)

--- lib/helpers/grid.dart
+++ lib/helpers/grid.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_cached_image/firebase_cached_image.dart';
